7a0c9afe44d00a99bbe6c86c8d773db7c3741384
wildhoney <hanggai@aoyou.com>
2016-09-09 15:23:20 +0800

    代码修改

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-require(["/component/pager/avalon.pager"],function () {
+require(["/component/base/mmRequest","/component/pager/avalon.pager"],function (req) {
     var vm = avalon.define({
         $id:"aoyouimage",
         filtertoggle:false,
@@ -8,18 +8,42 @@ require(["/component/pager/avalon.pager"],function () {
         changeperPage:function(p){//切换每页显示多少条
           avalon.vmodels['pager01'].perPages = p;
         },
-        imagedata:[
-            {name:"米兰大教堂",type:"JPG",size:"1920*1280",space:"1.6M",lastupdate:"2016-08-19 12:07",check:"unchecked"},
-            {name:"米兰大教堂",type:"JPG",size:"1920*1280",space:"1.6M",lastupdate:"2016-08-19 12:07",check:"unchecked"},
-            {name:"米兰大教堂",type:"JPG",size:"1920*1280",space:"1.6M",lastupdate:"2016-08-19 12:07",check:"unchecked"}
-        ],
+        imagedata:[],
         checkimg:function(idx){
             vm.imagedata[idx]['check'] = (vm.imagedata[idx]['check'] == "unchecked"?"checked":"unchecked");
         },
+        checkallclass:"unchecked",
         checkallimg:function(){
-            for(var i=0,j=vm.imagedata.length;i<j;i++){
-                vm.imagedata[i]['check'] = 'checked';
+            if(vm.checkallclass == 'unchecked'){
+                vm.checkallclass = 'checked'
+                for(var i=0,j=vm.imagedata.length;i<j;i++){
+                    vm.imagedata[i]['check'] = 'checked';
+                }
+            }else{
+                vm.checkallclass = 'unchecked'
+                for(var i=0,j=vm.imagedata.length;i<j;i++){
+                    vm.imagedata[i]['check'] = 'unchecked';
+                }
             }
+
+        },
+        getImgInfo:function(url,para){
+            req.ajax({
+                type: 'POST',
+                url: url,
+                data: para,
+                headers: {},
+                success: function(dat, status, xhr) {
+                    var redata = avalon.parseJSON(dat);
+                    if(redata){
+                        vm.imagedata = redata.data;
+                        avalon.vmodels['pager01'].totalItems = redata.total;
+                    }
+                },
+                error: function(dat) {
+                    console.log('没有获取数据');
+                }
+            });
         },
         $pageropt:{
             showPages:0,
@@ -31,8 +55,8 @@ require(["/component/pager/avalon.pager"],function () {
                 return "<span class='pagershow'>显示</span><div class='pagersel' ms-hover='on'>{{perPages}}<ul><li ms-click='changeperPage(el.text)' ms-repeat='options'>{{el.text}}</li></ul></div>" + tmpl
             },
             options:[20,50,100],
-            onJump:function(){
-                alert(10);
+            onJump:function(ev,pg){
+                vm.getImgInfo('../json/imginfo.json',{'pagenum':pg._currentPage});
             }
         },
         filteron:function (e) {
@@ -50,6 +74,7 @@ require(["/component/pager/avalon.pager"],function () {
             vm.tablist = val;
         }
     });
+    vm.getImgInfo('../json/imginfo.json',{'pagenum':0});
     avalon.scan(document.body,vm);
 })
 
